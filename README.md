2ba6ec5c77d4be03abcd107529ce8d78c1723e5d
Jgonz156 <Jgonz156@lion.lmu.edu>
2023-09-15 19:29:59 -0700

    finished changemaker in js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CMSI-3801-Recitation-Problems
+
+This is the codebase responsible for holding the javascript solution to the changemaker problem.
---
+++ src/changeMaker.js
@@ -0,0 +1,12 @@
+export function change(amount) {
+  if (amount < 0) throw new RangeError("amount cannot be negative")
+  let [coins, remaining] = [[], amount]
+  for (let denomination of [25, 10, 5]) {
+    coins.push(Math.floor(remaining / denomination))
+    remaining %= denomination
+  }
+  coins.push(remaining)
+  return coins
+}
+
+export default change
